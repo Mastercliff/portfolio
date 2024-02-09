823dfbc24f2006ead2b71863530ef1e525cb0f0c
Mastercliff <egarrido9898@gmail.com>
2024-02-08 23:52:22 -0300

    fix: background color

--- src/constants/theme-constants.ts
+++ src/constants/theme-constants.ts
@@ -1,1 +1,1 @@
-export const baseInterfaceClasses = "rounded-md text-white ring-1 ring-gray-800"
+export const baseInterfaceClasses = "rounded-md text-white ring-1 backdrop-blur-lg bg-blend-multiply"
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,7 @@
 import Profile from "@/components/layout/Profile";
 
 export default function MainPage() {
-	return <main className="flex flex-row p-4 backdrop-blur-md bg-blend-multiply" style={{
+	return <main className="flex flex-row p-4" style={{
 		height: "100vh"
 	}}>
 		<div className="h-full">
